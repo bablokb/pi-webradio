1470ff50e24e82dbb645cd2a9f50ac05cecbdacb
Bernhard Bablok <bablokb@gmx.de>
2021-06-14 06:32:12 +0200

    import SRWeb into webradio-namespace

--- files/usr/local/lib/pi-webradio/__init__.py
+++ files/usr/local/lib/pi-webradio/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
-# Pi-Webradio application module.
+# Pi-Webradio application module. The file just imports all classes
+# into the wstation namespace
 #
 # Author: Bernhard Bablok
 # License: GPL3
@@ -9,3 +10,5 @@
 # Website: https://github.com/bablokb/pi-webradio
 #
 # ----------------------------------------------------------------------------
+
+from . SRWeb           import SRWeb           as SRWeb
