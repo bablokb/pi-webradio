06eeb25da39b1e1ac0c6e2180a9aef9b53eb35b4
Bernhard Bablok <bablokb@gmx.de>
2021-06-15 19:09:23 +0200

    moved application class to SRWebRadio

--- files/usr/local/bin/pi-webradio.py
+++ files/usr/local/bin/pi-webradio.py
@@ -10,13 +10,10 @@
 #
 # ----------------------------------------------------------------------------
 
-VERSION      = 0.1
-DEFAULT_PORT = 8026
+VERSION = "0.1"
 
-# --- system  imports   ------------------------------------------------------
-
-import os, sys
-from argparse import ArgumentParser
+import locale, os, sys, signal
+from   argparse import ArgumentParser
 
 # --- application imports   --------------------------------------------------
 
@@ -30,41 +27,73 @@ from webradio import *
 class Options(object):
   pass
 
-# --- commandline-parser   --------------------------------------------------
+# --- cmdline-parser   ------------------------------------------------------
 
 def get_parser():
+  """ configure cmdline-parser """
+
   parser = ArgumentParser(add_help=False,
-    description='Webinterface for the pi-webradio project')
+    description='Pi-Webradio (version: %s)' % VERSION)
 
-  parser.add_argument('-H', '--host', nargs=1,
-    metavar='host', default=['0.0.0.0'],
-    dest='host',
-    help='host-mask')
-  parser.add_argument('-P', '--port', nargs=1,
-    metavar='port', default=[DEFAULT_PORT],
-    dest='port',
-    help='port the server is listening on (default: %d)' % DEFAULT_PORT)
+  parser.add_argument('-p', '--play', action='store_true',
+    dest='do_play', default=True,
+    help="play radio (default)")
 
-  parser.add_argument('-d', '--debug',
-    dest='debug', default=False, action='store_true',
-    help='start in debug-mode')
+  parser.add_argument('-l', '--list', action='store_true',
+    dest='do_list', default=False,
+    help="display radio-channels")
+
+  parser.add_argument('-r', '--record', action='store_true',
+    dest='do_record', default=False,
+    help="record radio (needs channel as argument)")
+  parser.add_argument('-t', '--tdir', nargs=1,
+    metavar='target directory', default=None,
+    dest='target_dir',
+    help='target directory for recordings')
 
   parser.add_argument('-h', '--help', action='help',
-    help='display this help')
+    help='print this help')
+
+  parser.add_argument('channel', nargs='?', metavar='channel',
+    default=None, help='channel number')
+  parser.add_argument('duration', nargs='?', metavar='duration',
+    default=0, help='duration of recording')
   return parser
 
-# --- main program   --------------------------------------------------------
+# --- validate and fix options   ---------------------------------------------
+
+def check_options(options):
+  """ validate and fix options """
+
+  # record needs a channel number
+  if options.do_record and not options.channel:
+    print("[ERROR] record-option (-r) needs channel nummber as argument")
+    sys.exit(3)
+
+# --- main program   ----------------------------------------------------------
 
 if __name__ == '__main__':
-  # read options
-  opt_parser = get_parser()
-  options = opt_parser.parse_args(namespace=Options)
+
+  # set local to default from environment
+  locale.setlocale(locale.LC_ALL, '')
+
+  # parse commandline-arguments
+  opt_parser     = get_parser()
+  options        = opt_parser.parse_args(namespace=Options)
   options.version = VERSION
   options.pgm_dir = os.path.dirname(os.path.abspath(__file__))
-  if options.debug:
-    print("DEBUG: running version:   %s" % options.version)
-    print("DEBUG: pgm_dir directory: %s" % options.pgm_dir)
+  check_options(options)
 
-  # start server
-  server = SRWeb(options)
-  server.run()
+  app = WebRadio(options)
+
+  # setup signal-handler
+  signal.signal(signal.SIGTERM, app.signal_handler)
+  signal.signal(signal.SIGINT,  app.signal_handler)
+
+  if options.do_list:
+    app.radio.print_channels()
+  elif options.do_record:
+    app.recorder.record(app.radio.get_channel(int(options.channel)-1))
+  else:
+    app.run()
+    signal.pause()
