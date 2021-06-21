52fa2b1c46dedb6c6bd3663c7575a063f1c1400d
Bernhard Bablok <bablokb@gmx.de>
2021-06-21 19:26:02 +0200

    add Mpg123

--- files/usr/local/lib/webradio/__init__.py
+++ files/usr/local/lib/webradio/__init__.py
@@ -15,5 +15,6 @@ from . SRBase           import Base           as Base
 from . SRApi           import Api           as Api
 from . SRRadio           import Radio           as Radio
 from . SRRecorder           import Recorder           as Recorder
+from . SRMpg123           import Mpg123           as Mpg123
 from . SRWebRadio           import WebRadio           as WebRadio
 #from . SRWebServer          import WebServer          as WebServer
