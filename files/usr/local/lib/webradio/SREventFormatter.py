9019783aa695c8601075144eeb122c6632894a1f
Bernhard Bablok <bablokb@gmx.de>
2021-07-04 07:37:13 +0200

    initial commit

---
+++ files/usr/local/lib/webradio/SREventFormatter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Pi-Webradio: implementation of class EventFormatter
+#
+# The class EventFormatter converts events to a printable form.
+# TODO: support i18n
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-webradio
+#
+# -----------------------------------------------------------------------------
+
+class EventFormatter(object):
+  """ format events """
+
+  # --- format map: type:format   ---------------------------------------------
+  _FMT_MAP = {
+    'icy-meta': '{value}',
+    'icy-name': 'current station: {value}',
+    'rec_start': 'recording {name} for {duration} minutes',
+    'rec_stop': 'finished recording. File {filename}, duration: {duration}m',
+    'vol_set': 'setting current volume to {value}',
+    'radio_play_channel': 'start playing channel {nr} ({name})'
+    }
+
+  # --- format event   --------------------------------------------------------
+
+  def format(self,event):
+    """ format given event """
+
+    key = event['type']
+    if key in EventFormatter._FMT_MAP:
+      if isinstance(event['value'],dict):
+        return EventFormatter._FMT_MAP[key].format(**event['value'])
+      else:
+        return EventFormatter._FMT_MAP[key].format(**event)
+    else:
+      return "%r" % (event.value,)
