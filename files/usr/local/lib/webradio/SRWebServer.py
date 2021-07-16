4e6f8cc479caf0e189ea7cb747e3ca69f468bca3
Bernhard Bablok <bablokb@gmx.de>
2021-07-16 15:52:14 +0200

    bottle->flask migration, part 1

--- files/usr/local/lib/webradio/SRWebServer.py
+++ files/usr/local/lib/webradio/SRWebServer.py
@@ -12,48 +12,12 @@
 
 # --- System-Imports   -------------------------------------------------------
 
-import os, bottle, json, queue, traceback
-from bottle import route, ServerAdapter
+import os, json, queue, traceback
+
+from flask import Flask, render_template as template, request, make_response
+from werkzeug.serving import make_server
 
 from webradio import Base
-
-# --- helper class to enable stopping   --------------------------------------
-# (this works around a shortcoming of Bottle)
-
-class StoppableServer(ServerAdapter):
-
-  def run(self, app): # pragma: no cover
-    from wsgiref.simple_server import WSGIRequestHandler, WSGIServer
-    from wsgiref.simple_server import make_server
-    import socket
-
-    class FixedHandler(WSGIRequestHandler):
-      def address_string(self): # Prevent reverse DNS lookups please.
-        return self.client_address[0]
-      def log_request(*args, **kw):
-        if not self.quiet:
-          return WSGIRequestHandler.log_request(*args, **kw)
-
-    handler_cls = self.options.get('handler_class', FixedHandler)
-    server_cls  = self.options.get('server_class', WSGIServer)
-
-    if ':' in self.host: # Fix wsgiref for IPv6 addresses.
-      if getattr(server_cls, 'address_family') == socket.AF_INET:
-        class server_cls(server_cls):
-          address_family = socket.AF_INET6
-
-    self._server = make_server(self.host, self.port, app, server_cls, handler_cls)
-    self._server.serve_forever()
-
-  def stop(self):
-    if hasattr(self,'_server'):
-      self._server.server_close()
-      self._server.shutdown()
-
-# ----------------------------------------------------------------------------
-# ----------------------------------------------------------------------------
-
-# --- application web-server class   -----------------------------------------
 
 class WebServer(Base):
   """ Serve GUI and process API-requests """
@@ -91,13 +55,13 @@ class WebServer(Base):
     """ set up routing (decorators don't seem to work for methods within a class """
 
     # web-interface (GUI)
-    bottle.get('/',callback=self.main_page)
-    bottle.get('/css/<filepath:path>',callback=self.css_pages)
-    bottle.get('/webfonts/<filepath:path>',callback=self.webfonts)
-    bottle.get('/images/<filepath:path>',callback=self.images)
-    bottle.get('/js/<filepath:path>',callback=self.js_pages)
-    bottle.get('/api/get_events',callback=self.get_events)
-    bottle.get('/api/<api:path>',callback=self.process_api)
+#     bottle.get('/',callback=self.main_page)
+#     bottle.get('/css/<filepath:path>',callback=self.css_pages)
+#     bottle.get('/webfonts/<filepath:path>',callback=self.webfonts)
+#     bottle.get('/images/<filepath:path>',callback=self.images)
+#     bottle.get('/js/<filepath:path>',callback=self.js_pages)
+#     bottle.get('/api/get_events',callback=self.get_events)
+#     bottle.get('/api/<api:path>',callback=self.process_api)
 
   # --- return absolute path of web-files   ----------------------------------
 
@@ -107,23 +71,23 @@ class WebServer(Base):
 
   # --- static routes   ------------------------------------------------------
 
-  def css_pages(self,filepath):
-    return bottle.static_file(filepath, root=self._get_path('css'))
+#   def css_pages(self,filepath):
+#     return bottle.static_file(filepath, root=self._get_path('css'))
 
-  def webfonts(self,filepath):
-    return bottle.static_file(filepath, root=self._get_path('webfonts'))
+#   def webfonts(self,filepath):
+#     return bottle.static_file(filepath, root=self._get_path('webfonts'))
   
-  def images(self,filepath):
-    return bottle.static_file(filepath, root=self._get_path('images'))
+#   def images(self,filepath):
+#     return bottle.static_file(filepath, root=self._get_path('images'))
   
-  def js_pages(self,filepath):
-    return bottle.static_file(filepath, root=self._get_path('js'))
+#   def js_pages(self,filepath):
+#     return bottle.static_file(filepath, root=self._get_path('js'))
   
   # --- main page   ----------------------------------------------------------
 
-  def main_page(self):
-    tpl = bottle.SimpleTemplate(name="index.html",lookup=[self._web_root])
-    return tpl.render()
+#   def main_page(self):
+#     tpl = bottle.SimpleTemplate(name="index.html",lookup=[self._web_root])
+#     return tpl.render()
 
   # --- update logo with correct path   ------------------------------------
 
@@ -144,14 +108,14 @@ class WebServer(Base):
       # internal API, illegal request!
       self.msg("illegal api-call: %s" % api)
       msg = '"illegal request /api/%s" % api'
-      bottle.response.content_type = 'application/json'
-      bottle.response.status       = 400                 # bad request
+#       bottle.response.content_type = 'application/json'
+#       bottle.response.status       = 400                 # bad request
       return '{"msg": ' + msg +'}'
     else:
       self.msg("processing api-call: %s" % api)
       try:
-        response = self._api.exec(api,**bottle.request.query)
-        bottle.response.content_type = 'application/json'
+#         response = self._api.exec(api,**bottle.request.query)
+#         bottle.response.content_type = 'application/json'
         if api in ['radio_play_channel','radio_get_channel']:
           self._update_logo(response)
         elif api == 'radio_get_channels':
@@ -160,15 +124,15 @@ class WebServer(Base):
       except NotImplementedError as err:
         self.msg("illegal request: /api/%s" % api)
         msg = '"/api/%s not implemented" % api'
-        bottle.response.content_type = 'application/json'
-        bottle.response.status       = 400                 # bad request
+#         bottle.response.content_type = 'application/json'
+#         bottle.response.status       = 400                 # bad request
         return '{"msg": ' + msg +'}'
       except Exception as ex:
         self.msg("exception while calling: /api/%s" % api)
         traceback.print_exc()
         msg = '"internal server error"'
-        bottle.response.content_type = 'application/json'
-        bottle.response.status       = 500                 # internal error
+#         bottle.response.content_type = 'application/json'
+#         bottle.response.status       = 500                 # internal error
         return '{"msg": ' + msg +'}'
 
   # --- stream SSE (server sent events)   ----------------------------------
@@ -180,24 +144,24 @@ class WebServer(Base):
     self._api._add_consumer("web",ev_queue)
 
     try:
-      bottle.response.content_type = 'text/event-stream'
-      bottle.response.set_header('Cache-Control','no-cache')
-      bottle.response.set_header('Connection','keep-alive')
+#       bottle.response.content_type = 'text/event-stream'
+#       bottle.response.set_header('Cache-Control','no-cache')
+#       bottle.response.set_header('Connection','keep-alive')
       while True:
         ev = ev_queue.get()
         ev_queue.task_done()
         if ev:
           sse = "data: %s\n\n" % json.dumps(ev)
           self.msg("WebServer: serving event %s" % sse)
-          bottle.response.status = 200                 # OK
+#           bottle.response.status = 200                 # OK
           # send data using SSE
           yield sse
         else:
           break
     except:
       traceback.print_exc()
-  bottle.response.content_type = 'text/plain'
-  bottle.response.status = 404
+#   bottle.response.content_type = 'text/plain'
+#   bottle.response.status = 404
 
   # --- stop web-server   --------------------------------------------------
 
@@ -205,26 +169,20 @@ class WebServer(Base):
     """ stop the web-server """
 
     self.msg("WebServer: process stop-request")
-    self._server.stop()
+    self._server.shutdown()
 
   # --- service-loop   -----------------------------------------------------
 
   def run(self):
     """ start and run the webserver """
 
-    self._server = StoppableServer(host=self._host,port=self._port)
-    if self.debug:
-      self.msg("WebServer: starting the web-server in debug-mode")
-      self.msg("WebServer: listening on port %s" % self._port)
-      self.msg("WebServer: using web-root: %s" % self._web_root)
-      bottle.run(host='localhost',
-                 port=self._port,
-                 server=self._server,
-                 debug=True,reloader=False)
-    else:
-      bottle.run(host=self._host,
-                 port=self._port,
-                 server=self._server,
-                 debug=False,quiet=True,reloader=False)
+    app = Flask('pi-webradio')
+    self._server = make_server(self._host,self._port,app)
+    ctx = app.app_context()
+    ctx.push()
 
+    self.msg("WebServer: starting the web-server in debug-mode")
+    self.msg("WebServer: listening on port %s" % self._port)
+    self.msg("WebServer: using web-root: %s" % self._web_root)
+    self._server.serve_forever()
     self.msg("WebServer: finished")
