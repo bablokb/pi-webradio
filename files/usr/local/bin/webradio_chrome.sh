7fc071b216b689466f2a7208e057d78f8cc3254b
Bernhard Bablok <bablokb@gmx.de>
2021-07-24 11:59:46 +0200

    initial commit

---
+++ files/usr/local/bin/webradio_chrome.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# --------------------------------------------------------------------------
+# This starts chrome in app-mode and connects to the local server.
+#
+# The script monitors the status of the pi-webradio service and if the
+# service terminates it will also kill chrome.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-webradio
+#
+# --------------------------------------------------------------------------
+
+chromium-browser --app=http://localhost:8026/ --kiosk &
+pid_cb="$!"
+
+# monitor systemd-service
+while systemctl --quiet is-active pi-webradio.service; do
+  sleep 5
+done
+
+# kill browser
+kill -15 "$pid_cb"
