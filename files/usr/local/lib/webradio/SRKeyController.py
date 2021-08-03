b23896ca4e871568b456b1cda16b4959deb3b062
Bernhard Bablok <bablokb@gmx.de>
2021-08-03 20:17:17 +0200

    implement key-mapping from terminal-input

--- files/usr/local/lib/webradio/SRKeyController.py
+++ files/usr/local/lib/webradio/SRKeyController.py
@@ -12,7 +12,7 @@
 #
 # ----------------------------------------------------------------------------
 
-import evdev, select
+import os, sys, evdev, select, tty, termios
 from webradio import Base
 
 class KeyController(Base):
@@ -20,7 +20,7 @@ class KeyController(Base):
 
   # --- builtin-keymaps   ----------------------------------------------------
 
-  KEYMAP_RADIO = {
+  KEYMAP_RADIO_EVENT = {
     "KEY_1":     ["radio_play_channel", "nr=1"],
     "KEY_2":     ["radio_play_channel", "nr=2"],
     "KEY_3":     ["radio_play_channel", "nr=3"],
@@ -47,6 +47,33 @@ class KeyController(Base):
     "KEY_ENTER": ["player_select"]                      # TBD
     }
 
+  KEYMAP_RADIO_TERM = {
+    "31":     ["radio_play_channel", "nr=1"],
+    "32":     ["radio_play_channel", "nr=2"],
+    "33":     ["radio_play_channel", "nr=3"],
+    "34":     ["radio_play_channel", "nr=4"],
+    "35":     ["radio_play_channel", "nr=5"],
+    "36":     ["radio_play_channel", "nr=6"],
+    "37":     ["radio_play_channel", "nr=7"],
+    "38":     ["radio_play_channel", "nr=8"],
+    "39":     ["radio_play_channel", "nr=9"],
+    "30":     ["radio_play_channel", "nr=10"],
+    "64":     ["player_play_dir"],                   # TBD
+    "66":     ["player_play_file"],                  # TBD
+    "69":     ["radio_state"],                       # TBD
+    "6f":     ["radio_toggle"],
+    "70":     ["player_mode_toggle"],                # TBD
+    "71":     ["sys_stop"],
+    "72":     ["rec_toggle"],
+    "6c":     ["radio_get_channels"],
+    "6d":     ["vol_mute_toggle"],
+    "1b5b44": ["radio_play_prev"],
+    "1b5b43": ["radio_play_next"],
+    "1b5b41": ["vol_up"],
+    "1b5b42": ["vol_down"],
+    "0a":     ["player_select"]                      # TBD
+    }
+
   KEY_SPECIAL = ['KEY_LEFTCTRL','KEY_LEFTALT','KEY_LEFTSHIFT',
                  'KEY_RIGHTCTRL','KEY_RIGHTALT','KEY_RIGHTSHIFT']
 
@@ -56,15 +83,27 @@ class KeyController(Base):
     """ constructor """
 
     self._stop    = stop
-    self._kmap    = KeyController.KEYMAP_RADIO
     self.debug    = debug
+
+    # test for terminal
+    self._have_term = False
+    try:
+      _ = os.tcgetpgrp(sys.stdin.fileno())
+      self.msg("KeyController: have terminal")
+      self._have_term = True
+      self._kmap    = KeyController.KEYMAP_RADIO_TERM
+    except:
+      self._kmap    = KeyController.KEYMAP_RADIO_EVENT
 
   # --- yield api from key-event   -------------------------------------------
 
-  def api_from_key(self):
+  def _api_from_key_event(self):
     """ monitor key-events and yield mapped API-name """
 
     devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+    if not len(devices):
+      self.msg("no input devices available")
+      return
     devices = {dev.fd: dev for dev in devices}
 
     special = 0                    # to ignore combinations with special keys
@@ -95,3 +134,35 @@ class KeyController(Base):
           elif event.keystate == event.key_up:
             if event.keycode in KeyController.KEY_SPECIAL:
               special = max(0,special-1)
+
+  # --- yield api from key-event   -------------------------------------------
+
+  def _api_from_term(self):
+    """ monitor chars from terminal and yield mapped API-name """
+
+    old_settings = termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin.fileno())
+    try:
+      while not self._stop.is_set():
+        keycode = os.read(sys.stdin.fileno(), 3).hex()
+        self.msg("KeyController: processing %s" % keycode)
+        if keycode in self._kmap:
+          # key is mapped, yield api-name
+          self.msg("KeyController: mapping %s to %s" %
+                   (keycode,self._kmap[keycode]))
+          yield self._kmap[keycode]
+        else:
+          # key is not mapped, ignore
+          self.msg("KeyController: ignoring %s" % keycode)
+    finally:
+      termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+  # --- yield api from key-event   -------------------------------------------
+
+  def api_from_key(self):
+    """ monitor key-events and yield mapped API-name """
+
+    if self._have_term:
+      return self._api_from_term()
+    else:
+      return self._api_from_key_event()
