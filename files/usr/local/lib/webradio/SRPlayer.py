94f5bac0a93f015a651e222fdd0013afe8433865
Bernhard Bablok <bablokb@gmx.de>
2021-08-19 11:58:17 +0200

    implemented player-APIs

--- files/usr/local/lib/webradio/SRPlayer.py
+++ files/usr/local/lib/webradio/SRPlayer.py
@@ -19,6 +19,10 @@ from webradio import Base
 class Player(Base):
   """ Player-controller """
 
+  _STOP  = 0
+  _PLAY  = 1
+  _PAUSE = 2
+
   def __init__(self,app):
     """ initialization """
 
@@ -28,6 +32,7 @@ class Player(Base):
     self._backend = app.backend
 
     self._file    = None
+    self._state   = Player._STOP
 
     self.read_config()
     self.register_apis()
@@ -42,27 +47,30 @@ class Player(Base):
                                     "player_root_dir",
                                     os.path.expanduser("~"))
     if not os.path.exists(self._root_dir):
-      self.msg("WARNING: root-directory %s of player does not exist" %
+      self.msg("[WARNING] Player: root-directory %s of player does not exist" %
                self._root_dir,True)
       self._root_dir = os.path.expanduser("~")
-      self.msg("WARNING: using %s as fallback" % self._root_dir,True)
+      self.msg("[WARNING] Player: using %s as fallback" % self._root_dir,True)
     self._root_dir = os.path.abspath(self._root_dir)
 
     self._def_dir = self.get_value(self._app.parser,"PLAYER",
                                     "player_def_dir",
                                     self._root_dir)
     if not os.path.exists(self._def_dir):
-      self.msg("WARNING: default-directory %s of player does not exist" %
+      self.msg("[WARNING] Player: default-directory %s of player does not exist" %
                self._def_dir,True)
       self._def_dir = self._root_dir
-      self.msg("WARNING: using %s as fallback" % self._def_dir,True)
+      self.msg("[WARNING] Player: using %s as fallback" % self._def_dir,True)
     self._def_dir = os.path.abspath(self._def_dir)
 
     if not os.path.commonpath([self._root_dir,self._def_dir]) == self._root_dir:
-      self.msg("WARNING: default-directory is not below root-directory",True)
+      self.msg("[WARNING] Player: default-directory is not below root-directory",True)
       self._def_dir = self._root_dir
-      self.msg("WARNING: using %s as fallback" % self._def_dir,True)
+      self.msg("[WARNING] Player: using %s as fallback" % self._def_dir,True)
+
     self._dir = self._def_dir
+    self.msg("Player: root dir:    %s" % self._root_dir)
+    self.msg("Player: default dir: %s" % self._def_dir)
 
   # --- register APIs   ------------------------------------------------------
 
@@ -70,6 +78,10 @@ class Player(Base):
     """ register API-functions """
 
     self._api.player_play_file  = self.player_play_file
+    self._api.player_stop       = self.player_stop
+    self._api.player_pause      = self.player_pause
+    self._api.player_resume     = self.player_resume
+    self._api.player_toggle     = self.player_toggle
     self._api.player_select_dir = self.player_select_dir
 
   # --- return persistent state of this class   -------------------------------
@@ -109,14 +121,69 @@ class Player(Base):
   def player_play_file(self,fname=None):
     """ start playing """
 
+    if self._state == Player._PLAY:
+      self.player_stop()
+
     if fname:
-      self._file = fname
-    total_secs = int(subprocess.check_output(["mp3info", "-p","%S",self._file]))
+      if not os.path.isabs(fname):
+        fname = os.path.join(self._dir,fname)
+      if os.path.exists(fname):
+        self._file = fname
+      else:
+        raise ValueError("filename %s does not exist" % fname)
+
+    self._backend.play(self._file)
+    self._state = Player._PLAY
     self._api._push_event({'type': 'play_file', 'value': self._file})
+    total_secs = int(subprocess.check_output(["mp3info", "-p","%S",self._file]))
     self._api._push_event({'type': 'file_info',
                            'value': {'name': self._file,
                                      'duration': self._pp_time(total_secs)}})
-    self._backend.play(self._file)
+
+  # --- stop playing   -------------------------------------------------------
+
+  def player_stop(self):
+    """ stop playing (play->stop, pause->stop)"""
+
+    if self._state == Player._STOP:
+      return                      # nothing playing
+    else:
+      self._backend.stop()        # backend will publish eof-event
+      self._state = Player._STOP
+
+  # --- pause playing   -----------------------------------------------------
+
+  def player_pause(self):
+    """ pause playing (play->pause) """
+
+    if not self._state == Player._PLAY:
+      return                      # nothing playing
+    else:
+      self._backend.pause()
+      self._state = Player._PAUSE
+
+  # --- resume playing   ----------------------------------------------------
+
+  def player_resume(self):
+    """ resume playing (pause->play) """
+
+    if not self._state == Player._PAUSE:
+      return                      # not paused
+    else:
+      self._backend.resume()
+      self._state = Player._PLAY
+
+  # --- toggle playing   ------------------------------------------------------
+
+  def player_toggle(self):
+    """ toggle playing (play->pause, pause->play) """
+
+    if self._state == Player._STOP:
+      return                      # not paused
+    elif self._state == Player._PLAY:
+      self.player_pause()
+    else:
+      self.player_resume()
 
   # --- select directory, return entries   ------------------------------------
 
@@ -130,14 +197,15 @@ class Player(Base):
       # check path
       dir = os.path.abspath(dir)
       if not os.path.commonpath([self._root_dir,dir]) == self._root_dir:
-        self.msg("WARNING: directory %s is not below root-directory" % dir,True)
+        self.msg("[WARNING] Player: directory %s is not below root-directory" % dir,True)
         return result
       elif not os.path.isdir(dir):
-        self.msg("WARNING: directory %s does not exist" % dir,True)
+        self.msg("[WARNING] Player: directory %s does not exist" % dir,True)
         return result
       self._dir = dir
 
     # iterate over directory ...
+    self.msg("Player: collecting dir-info for %s" % dir)
     result['dirs'].append('..')
     for f in os.listdir(dir):
       if os.path.isfile(os.path.join(dir,f)):
