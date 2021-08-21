3b1d502e4f3ff37806c49ed4df302ddc0eefa271
Bernhard Bablok <bablokb@gmx.de>
2021-08-21 14:51:38 +0200

    dynamically query list of APIs

--- files/usr/local/lib/webradio/SRRadioClient.py
+++ files/usr/local/lib/webradio/SRRadioClient.py
@@ -12,7 +12,7 @@
 #
 # ----------------------------------------------------------------------------
 
-import urllib3, threading, time
+import urllib3, threading, time, json
 import sseclient
 import http.client as httplib
 
@@ -20,19 +20,6 @@ from webradio import Base
 
 class RadioClient(Base):
   """ python-client for the webradio-api """
-
-  API_LIST = [
-    "get_version", "sys_restart", "sys_stop", "sys_reboot", "sys_halt",
-    "radio_state", "radio_on", "radio_off", "radio_toggle",
-    "radio_get_channels", "radio_get_channel", "radio_play_channel",
-    "radio_play_next", "radio_play_prev", "vol_up", "vol_down",
-    "vol_set", "vol_mute_on", "vol_mute_off", "vol_mute_toggle",
-    "rec_start", "rec_stop", "rec_toggle", "player_mode_start",
-    "player_mode_exit", "player_mode_toggle", "player_play_file",
-    "player_play_dir", "player_stop", "player_pause", "player_toggle",
-    "player_select_dir", "player_next", "player_prev", "player_delete",
-    "get_events", "publish_state"
-    ]
 
   # --- constructor   --------------------------------------------------------
 
@@ -46,6 +33,7 @@ class RadioClient(Base):
     self._sseclient = None
     self._stop      = threading.Event()
     self._have_ev   = False
+    self._api_list  = None
 
   # --- close request object   -----------------------------------------------
 
@@ -115,6 +103,22 @@ class RadioClient(Base):
     except Exception as ex:
       self.msg("RadioClient: exception: %s" % ex)
       return None
+
+  # --- query API-list   -----------------------------------------------------
+
+  def get_api_list(self):
+    """ query API list """
+
+    if self._api_list:
+      return self._api_list
+    else:
+      _1,_2,apis = self.exec("get_api_list")
+      self.msg("RadioClient: API-list: %r" % (apis,))
+      if apis:
+        self._api_list = json.loads(apis)
+        return self._api_list
+      else:
+        return ""
 
   # --- process events   -----------------------------------------------------
 
