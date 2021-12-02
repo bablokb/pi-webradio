80f813fb6262520fe4c237eb16c9512af4ca759e
Bernhard Bablok <bablokb@gmx.de>
2021-12-02 07:10:05 +0100

    initial commit

---
+++ tools/word_map_de.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# German phrase-api map.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-webradio
+#
+# ----------------------------------------------------------------------------
+
+words_de = {
+  "on":          "an",
+  "off":         "aus",
+  "volume up":   "lauter",
+  "volume down": "leiser",
+  "channel":     "kanal",
+  "next":        "weiter",
+  "previous":    "zurück",
+  "stop":        "stop",
+  "quit":        "ende",
+  "radio":       "radio",
+  1:             "eins",
+  2:             "zwei",
+  3:             "drei",
+  4:             "vier",
+  5:             "fünf",
+  6:             "sechs",
+  7:             "sieben",
+  8:             "acht",
+  9:             "neun",
+  10:            "zehn",
+  11:            "elf",
+  12:            "zwölf",
+  13:            "dreizehn",
+  14:            "vierzehn",
+  15:            "fünfzehn",
+  16:            "sechzehn",
+  17:            "siebzehn",
+  18:            "achtzehn",
+  19:            "neunzehn",
+  20:            "zwanzig",
+  24:            "vier und zwanzig"
+  }
---
+++ tools/word_map_en.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# English phrase-api map.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-webradio
+#
+# ----------------------------------------------------------------------------
+
+words_en = {
+  "on":          "on",
+  "off":         "off",
+  "volume up":   "volume up",
+  "volume down": "volume down",
+  "channel":     "channel",
+  "next":        "next",
+  "previous":    "previous",
+  "stop":        "stop",
+  "quit":        "quit",
+  "radio":       "radio",
+  1:             "one",
+  2:             "two",
+  3:             "three",
+  4:             "four",
+  5:             "five",
+  6:             "six",
+  7:             "seven",
+  8:             "eight",
+  9:             "nine",
+  10:            "ten",
+  11:            "eleven",
+  12:            "twelve",
+  13:            "thirteen",
+  14:            "fourteen",
+  15:            "fifteen",
+  16:            "sixteen",
+  17:            "seventeen",
+  18:            "eighteen",
+  19:            "nineteen",
+  20:            "twenty",
+  24:            "twenty four"
+  }
